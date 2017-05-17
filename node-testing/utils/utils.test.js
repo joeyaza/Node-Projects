c18b9cc0dbef153df5c0879309e4e1ec8310739c
joeyaza <JAzadehdel@gmail.com>
2017-05-17 18:56:41 +0100

    started testing with mocha

---
+++ node-testing/utils/utils.test.js
@@ -0,0 +1,15 @@
+const utils = require('./utils');
+
+it('should add 2 numbers', () => {
+	let res = utils.add(33, 11);
+	if (res !== 44) {
+		throw new Error(`Expected 44, got ${res}.`)
+	}
+});
+
+it('should square a number', ()=> {
+	let res = utils.square(2);
+	if (res !== 4) {
+		throw new Error(`Expected 4, got ${res}.`)
+	}
+})
