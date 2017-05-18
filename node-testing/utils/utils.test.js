b2d4393cfb8acf3c9dd75b19dc377d1a3593ec59
joeyaza <JAzadehdel@gmail.com>
2017-05-18 19:29:42 +0100

    update to testing with mocha and assertions

--- node-testing/utils/utils.test.js
+++ node-testing/utils/utils.test.js
@@ -1,15 +1,37 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add 2 numbers', () => {
 	let res = utils.add(33, 11);
-	if (res !== 44) {
-		throw new Error(`Expected 44, got ${res}.`)
-	}
+	expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', ()=> {
 	let res = utils.square(2);
-	if (res !== 4) {
-		throw new Error(`Expected 4, got ${res}.`)
-	}
-})
+	expect(res).toBe(4).toBeA('number');
+});
+
+// it('should expect some values', () => {
+// 	// expect({name: 'Joe'}).toEqual({name: 'Joe'});
+
+// 	// expect([2,3,4]).toExclude(1);
+
+// 	expect({
+// 		name: 'Joe',
+// 		age: 30,
+// 		location: 'London'
+// 	}).toExclude({
+// 		age: 26
+// 	})
+// });
+
+it('should verify first and last names', ()=> {
+	var user = {location: "London", age: 30};
+	var res = utils.setName(user, 'Joe Az');
+
+	expect(res).toInclude({
+		firstName: 'Joe',
+		lastName: 'Az'
+	});
+
+});
