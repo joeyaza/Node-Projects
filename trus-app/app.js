95fa5dd07d0f8ebc350de7408cac4bbdee018cdd
joeyaza <JAzadehdel@gmail.com>
2017-07-15 14:39:54 +0100

    state sorted - needs tidy up'

--- trus-app/app.js
+++ trus-app/app.js
@@ -4,8 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
+var session = require('express-session');
 
 var app = express();
 
@@ -13,15 +12,63 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret:'SECRET27265378TOKEN'}));
 
-app.use('/', index);
+/* GET home page. */
+app.get('/', function(req, res, next) {
+	if (req.session.numeric) {
+  res.render('index', { title: 'TRUSSLE', sesh: req.session.numeric  });
+} else 
+	res.render('index', { title: 'TRUSSLE' });
+});
+
+/* POST home page. */
+app.post('/', function(req, res, next) {
+  req.session.numeric = req.body.numeric;
+  var v = req.body.numeric;  
+  var hasNumber = /\d/;
+  if (!v) {
+  	console.log(req.body.numeric)
+  	z = "Please type something ! Remember, got to be a number or space, above 28 for a wordspace.";
+  	sesh = req.body.numeric;
+   	res.render('index', { output: z, sesh:req.body.numeric });
+  }
+  if (!hasNumber.test(v)) {
+  	z = "Sorry, please only use numbers and spaces";
+  	res.render('index', { output: z, sesh:req.body.numeric });
+  } else {
+	const anum={
+		1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h',
+		9: 'i', 10: 'j', 11: 'k', 
+		12: 'l', 13: 'm', 14: 'n',15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 
+		21: 'u', 22: 'v', 23: 'w', 24: 'x', 25: 'y', 26: 'z'}
+    let y = [];
+    let z = [];
+    var x = v.split(' ').map(Number);
+    for (i = 0; i < x.length; i++) {
+      if (x[i] < 26) {
+        y.push(x[i]);
+      } else {
+        while (x[i] > 26) {
+          x[i] = x[i]/ 27;
+        }
+        y.push(x[i]);
+      }
+    }
+    for (k=0; k< y.length; k++) {
+      z.push(anum[y[k]] || ' ')
+    }
+    z = z.join("");
+    res.render('index', { title: 'TRUSSLE', output: z });
+  }	
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
