08f9a0c9d48eb1a53d5f1efed948424aa3ea67b8
joeyaza <JAzadehdel@gmail.com>
2017-07-22 12:47:24 +0100

    update to user login

--- trus-app/README.md
+++ trus-app/README.md
@@ -1,18 +1,19 @@
 # THE DECODER
 
-## Install
-
-Run NPM install from the command line.
-Use Nodemon or NPM run start to start the app, and in browser visit http://localhost:4000/
-
 ## Build
 
 The app is built in Node.js using Express.js.
 
+## Install
+
+Run NPM install from the command line.
+Use Nodemon or NPM start to start the app, and in browser visit http://localhost:4000/
+
+
 ## Usage
 
 Type in numbers and spaces to decode into words. Press submit on completion.
-The app will show an error if any character other than a number or space is followed.
+The app will show an error if any character other than a number or space is input.
 
 ## Tests
 
--- user-login/routes/users.js
+++ user-login/routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -17,5 +19,76 @@ router.get('/login', function(req, res, next) {
   	'title': 'Login'
   })
 });
+
+router.post('/register', function(req, res, next) {
+	// get form vals
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	// check for img 
+	if(req.files) {
+		console.log('uploading..');
+		var profileImageOriginalName = req.files.profileimage.originalname;
+		var profileImageName = req.files.profileimage.name;
+		var profileImageMime = req.files.profileimage.mimetype;
+		var profileImagePath = req.files.profileimage.path;
+		var profileImageExt = req.files.profileimage.extension;
+		var profileImageSize = req.files.profileimage.size;
+	} else {
+		//set default image
+		var profileImageName = 'noimage.png';
+	}
+
+	// Form Validation
+	req.checkBody('name', 'Name field is required').notEmpty();
+	req.checkBody('email', 'Email field is required').notEmpty();
+	req.checkBody('email', 'Email Required').isEmail();
+	req.checkBody('username', 'Username field is required').notEmpty();
+	req.checkBody('password', 'Password field is required').notEmpty();
+	req.checkBody('password2', 'Make sure passwords match!').equals(req.body.password);
+
+	// Check for Errors
+
+	var errors = req.validationErrors();
+
+	if(errors) {
+		res.render('register',{
+			errors: errors,
+			name: name,
+			email: email,
+			username: username,
+			password: password,
+			password2: password2
+		});
+	} else {
+		var newUser = new User({
+			errors: errors,
+			name: name,
+			email: email,
+			username: username,
+			password: password,
+			profileimage: profileImageName
+		});
+
+		// create the user in database
+		User.createUser(newUser, function(err, user) {
+			if (err) throw err;
+			console.log(user);
+		});
+
+		// send message to user to say has been created
+		req.flash('success', 'You are now registered! Thanks!');
+
+		res.location('/');
+		res.redirect('/');
+	}
+
+});
+
+
+
 
 module.exports = router;
