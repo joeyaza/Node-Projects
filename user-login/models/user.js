1a21d5d0b1d2c37c27ed770915a36f4f02ec04a9
joeyaza <JAzadehdel@gmail.com>
2017-07-23 11:08:29 +0100

    continued work on passport and bcrpt - login in success

--- user-login/models/user.js
+++ user-login/models/user.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
 mongoose.connect('mongodb://localhost/nodeauth');
 var db = mongoose.connection;
 
@@ -10,7 +12,9 @@ var UserSchema = mongoose.Schema({
 		index: true
 	},
 	password: {
-		type: String
+		type: String, 
+		required: true,
+		bcrypt: true
 	},
 	email: {
 		type: String
@@ -25,6 +29,30 @@ var UserSchema = mongoose.Schema({
 
 var User = module.exports = mongoose.model('User', UserSchema); 
 
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+		if(err) return callback(err);
+		callback(null, isMatch);
+	});
+}
+
+
+module.exports.getUserById = function(id, callback) {
+	User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback) {
+	var query = {username: username};
+	User.findOne(query, callback);
+}
+
+
 module.exports.createUser = function(newUser, callback) {
-	newUser.save(callback);
+	bcrypt.hash(newUser.password, 10, function(error, hash){
+		if(error) throw error;
+		// set hashed password
+		newUser.password = hash;
+		// create user 
+		newUser.save(callback);
+	});
 }
--- user-login/routes/users.js
+++ user-login/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/user');
 
 /* GET users listing. */
@@ -85,7 +86,50 @@ router.post('/register', function(req, res, next) {
 		res.location('/');
 		res.redirect('/');
 	}
+});
 
+passport.serializeUser(function(user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+	User.getUserById(id, function(err, user) {
+		done(err, user);
+	});
+});
+
+passport.use(new LocalStrategy(
+	function(username, password, done){
+		User.getUserByUsername(username, function(err, user) {
+			if(err) throw (err);
+			if(!user) {
+				console.log('unknown user');
+				return done(null, false, {
+					message: 'Unknown user'
+				});
+			}
+
+			User.comparePassword(password, user.password, function(err, isMatch) {
+				if(err) throw err;
+				if(isMatch) {
+					return done(null, user);
+				} else {
+					console.log('no match!');
+					return done(null, false, {message: 'invalid passer'})
+				}
+			});
+		});
+	}
+));
+
+
+router.post('/login', passport.authenticate('local', {failureRedirect: '/users/login', 
+failureFlash: 'invalid username or passer' }), function(req, res){
+	console.log('Authentication Successful');
+
+
+	req.flash('success', 'You are logged in');
+	res.redirect('/')
 });
 
 
